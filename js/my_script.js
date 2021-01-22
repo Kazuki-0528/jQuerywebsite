380eb1efe08f9c55b96aa92a66dedc7bf1f3a9d5
github raamenzurururu <reku9696@gmail.com>
2021-01-22 15:03:19 +0900

    [add]スライドショーの実装

--- js/my_script.js
+++ js/my_script.js
@@ -54,4 +54,27 @@ $(function () {
     },
     { offset: "60%" }
   );
+
+  // スライドショー
+  $("#nav_btn .next").click(function () {
+    $("#slide:not(:animated)").animate(
+      {
+        "margin-left": -1 * $("#slide li").width(),
+      },
+      function () {
+        // アニメーション完了時
+        $("#slide").append($("#slide li:first-child"));
+        $("#slide").css("margin-left", "0");
+      }
+    );
+  });
+
+  $("#nav_btn .prev").click(function () {
+    $("#slide:not(:animated)")
+      .prepend($("#slide li:last-child"))
+      .css("margin-left", -1 * $("#slide li").width())
+      .animate({
+        "margin-left": 0,
+      });
+  });
 });
